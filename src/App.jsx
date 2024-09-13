dcf060d5de091eec71147c38d369e745589fa633
Logan <logansailer0633@gamil.com>
2024-09-13 17:27:09 -0400

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/footer";
 
